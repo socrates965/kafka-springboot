35149ea31a9a6421b925e2b09a969e2d965d7666
socrates965 <socrates.sullivan@gmail.com>
2024-02-14 18:17:25 +0700

    add kafka config variable add consumer config security protocol add confluent interceptor

--- src/main/java/com/kafka/KafkaConsumerApplication.java
+++ src/main/java/com/kafka/KafkaConsumerApplication.java
@@ -4,6 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.StringDeserializer;
 // import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,14 +32,30 @@ public class KafkaConsumerApplication {
 
 		final Properties props = new Properties();
 
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // props.put(SaslConfigs.SASL_JAAS_CONFIG, String.format("%s required username=\"%s\" " + "password=\"%s\";", PlainLoginModule.class.getName(), kafkaUserName, kafkaPassword));
-        // props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-        // props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+		String kafkaServer = "localhost:9092";
+		String kafkaUserName = "";
+		String kafkaPassword = "";
+		String groupId = "test";
+		String topics = "test";
+
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        
+		// doesnt need to enable if local testing
+		// props.put(SaslConfigs.SASL_JAAS_CONFIG, String.format("%s required username=\"%s\" " + "password=\"%s\";", PlainLoginModule.class.getName(), kafkaUserName, kafkaPassword));
+        // props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        // props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
 
+		// interceptor configuration, exclusive for confluent platform
+        // props.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor");
+        // props.put("confluent.monitoring.interceptor.bootstrap.servers",kafkaServer);
+        // props.put("confluent.monitoring.interceptor.security.protocol","SASL_SSL");
+        // props.put("confluent.monitoring.interceptor.sasl.mechanism","PLAIN");
+        // props.put("confluent.monitoring.interceptor.sasl.jaas.config",String.format("%s required username=\"%s\" " + "password=\"%s\";", PlainLoginModule.class.getName(), "interceptor", "interceptor"));
+
+		// Get Hostname
 		try {
             String hostname = InetAddress.getLocalHost().getHostName();
             props.put(ConsumerConfig.CLIENT_ID_CONFIG, hostname);
@@ -47,7 +66,7 @@ public class KafkaConsumerApplication {
         }
 
 		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
-			consumer.subscribe(Collections.singletonList("test"));
+			consumer.subscribe(Collections.singletonList(topics));
 
 			while (true) {
 			    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
